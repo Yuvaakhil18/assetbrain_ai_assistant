d0da104ed6cf69aa8b9a2519943f426d9d28e8c3
Yuvaakhil <yuvaakhil18@gmail.com>
2026-07-15 10:32:57 +0530

    chore: Suppress Pylance import warning in api wrapper

--- api/index.py
+++ api/index.py
@@ -4,4 +4,4 @@ import sys
 # Add the backend directory to the Python path so imports work
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
 
-from main import app
+from main import app # type: ignore
